7297948a33bdcaf999cfc2af1c3b7afd22eb6816
Lolillkillu <szymon.parusmajor@gmail.com>
2025-04-28 22:29:09 +0200

    animations added to quizdetail

--- QuizzAppFrontend/src/app/components/quizdetail/quizdetail.component.ts
+++ QuizzAppFrontend/src/app/components/quizdetail/quizdetail.component.ts
@@ -3,13 +3,22 @@ import { QuizService } from '../../services/quiz.service';
 import { Quiz } from '../../models/quiz.model';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-quiz-detail',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './quizdetail.component.html',
-  styleUrls: ['./quizdetail.component.css']
+  styleUrls: ['./quizdetail.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
 export class QuizDetailComponent implements OnInit {
   quiz?: Quiz;
